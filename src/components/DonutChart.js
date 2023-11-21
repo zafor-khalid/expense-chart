9a03ac647c13dac1443c9ce9fd15ca31b17cfd97
zafor <codemen.zafor@gmail.com>
2023-11-22 02:29:34 +0600

    chart updated and card design updated

--- src/components/DonutChart.js
+++ src/components/DonutChart.js
@@ -2,21 +2,24 @@ import React, { useEffect, useState } from "react";
 import { generateRandomDeepColor } from "../utils/generateRandomDeepColor";
 import { convertToDegrees } from "../utils/convertToDegrees";
 import { convertToPercent } from "../utils/convertToPercent";
+import { capitalizeFirstLetter } from "../utils/capitalizeFirstLetter";
 
 const DonutChart = ({ data }) => {
   const [currentTab, setCurrentTab] = useState(data[0]);
   const [totalValue, setTotalValue] = useState(0);
   const [cssString, setCssString] = useState("");
+  const [chartArr, setChartArr] = useState([]);
 
   useEffect(() => {
-    const chartArr = convertObjectToArray(currentTab);
+    const arrForChart = convertObjectToArray(currentTab);
     let total =
-      chartArr.length > 0
-        ? chartArr.reduce((a, b) => a + b.value, 0).toFixed(2)
+      arrForChart.length > 0
+        ? arrForChart.reduce((a, b) => a + b.value, 0).toFixed(2)
         : 0;
     setTotalValue(total);
-    let cssStr = generateCssString(chartArr, total);
+    let cssStr = generateCssString(arrForChart, total);
     setCssString(cssStr);
+    setChartArr(arrForChart);
   }, [currentTab]);
 
   const generateCssString = (arr, total) => {
@@ -31,12 +34,13 @@ const DonutChart = ({ data }) => {
         item.endPercent = convertToPercent(item.endValue, total);
         item.startDegrees = convertToDegrees(item.startPercent);
         item.endDegrees = convertToDegrees(item.endPercent);
+        item.backgroundColor = generateRandomDeepColor();
 
         return items;
       }, [])
       .map((chart, index) => {
-        const { startDegrees, endDegrees } = chart;
-        return ` ${generateRandomDeepColor()} ${startDegrees}deg ${endDegrees}deg`;
+        const { startDegrees, endDegrees, backgroundColor } = chart;
+        return ` ${backgroundColor} ${startDegrees}deg ${endDegrees}deg`;
       })
       .join();
     return generatedStr;
@@ -51,43 +55,62 @@ const DonutChart = ({ data }) => {
     }
     return arr;
   };
+
+  console.log(chartArr);
   return (
     <div className="donut-chart-container">
-      <div>
+      <div className="tab">
         {data.map((el, idx) => (
-          <button key={idx} onClick={() => setCurrentTab(el)}>
+          <button
+            key={idx}
+            onClick={() => setCurrentTab(el)}
+            className={currentTab.period === el.period ? "btn-active" : ""}
+          >
             {el.period}
           </button>
         ))}
       </div>
-      {
-        <div className="chart-wrapper">
-          <svg
-            viewBox="0 0 100 100"
-            xmlns="http://www.w3.org/2000/svg"
-            className="chart-svg"
+
+      <div className="chart-wrapper">
+        <svg
+          viewBox="0 0 100 100"
+          xmlns="http://www.w3.org/2000/svg"
+          className="chart-svg"
+        >
+          <clipPath id="hole">
+            <path d="M 50 0 a 50 50 0 0 1 0 100 50 50 0 0 1 0 -100 v 18 a 2 2 0 0 0 0 64 2 2 0 0 0 0 -64" />
+          </clipPath>
+          <foreignObject
+            x="0"
+            y="0"
+            width="100"
+            height="100"
+            clipPath="url(#hole)"
           >
-            <clipPath id="hole">
-              <path d="M 50 0 a 50 50 0 0 1 0 100 50 50 0 0 1 0 -100 v 18 a 2 2 0 0 0 0 64 2 2 0 0 0 0 -64" />
-            </clipPath>
-            <foreignObject
-              x="0"
-              y="0"
-              width="100"
-              height="100"
-              clipPath="url(#hole)"
-            >
-              <div
-                className="chart-inner"
-                style={{
-                  background: `conic-gradient(${cssString})`,
-                }}
-              ></div>
-            </foreignObject>
-          </svg>
-          <div className="chart-label">{totalValue}</div>
-        </div>
-      }
+            <div
+              className="chart-inner"
+              style={{
+                background: `conic-gradient(${cssString})`,
+              }}
+            ></div>
+          </foreignObject>
+        </svg>
+        <div className="chart-label">{totalValue}</div>
+      </div>
+      <div className="details">
+        {chartArr.length > 0 &&
+          chartArr.map((el, idx) => (
+            <div key={el?.period || idx}>
+              <span
+                className="pill"
+                style={{ backgroundColor: el?.backgroundColor }}
+              ></span>
+              <span className="description">
+                {capitalizeFirstLetter(el.name)}
+              </span>
+            </div>
+          ))}
+      </div>
     </div>
   );
 };
--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import DonutChart from "../components/DonutChart";
 import "../styles/DonutChart.css";
+import "../styles/global.css";
 import expenseData from "../Json/expenseJson.json";
 
 export default function App() {
@@ -12,7 +13,11 @@ export default function App() {
         <title>expense-chart</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <DonutChart data={data} />;
+      <h1 className={styles.title}>Expense Chart</h1>
+      <div className={styles.card}>
+        <h2 className={styles["sub-title"]}>Expense Chart</h2>
+        <DonutChart data={data} />;
+      </div>
     </div>
   );
 }
